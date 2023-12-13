c1fb89b194479e8cb1b1cb2436bbcad360f2eef0
firoj <fkfiroj02@gmail.com>
2023-12-13 23:16:57 +0600

    seeding

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,17 +9,17 @@
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1 class="m-0">Dashboard</h1>
-          </div><!-- /.col -->
+          </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Dashboard v1</li>
             </ol>
-          </div><!-- /.col -->
+          </div>
         </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+      </div>
     </div>
-    <!-- /.content-header -->
+   
 
     <!-- Main content -->
     <section class="content">
